866186597017069986f615336bc463e6b554e66b
Jukdelya <135731074+Jukdelya@users.noreply.github.com>
2023-06-06 22:53:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+
+        int e = 5;
+        while (e >= 0) {
+            System.out.println(e);
+            e--;
+        }
+        int n = 10;
+        do {
+            System.out.println(n--);}
+            while (n > 4);
+    }
+}
